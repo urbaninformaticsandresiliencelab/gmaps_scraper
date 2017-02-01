5a51cd72154edb345f8043d7439e59b7616f33c9
ercas <edgarrimandocastro@gmail.com>
2017-02-01 15:23:41 -0500

    Added --scrape-details option to gmaps_subdivisions.py

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import gmaps_subdivisions
 import glob
 
-#details = gmaps_subdivisions.DetailScraper("2016-12-29_detail_of_2016-12-23_San_Francisco_California.json", 5)
-#details.scrape("output/json/2016-12-23_San_Francisco_California.json")
+details = gmaps_subdivisions.DetailScraper("2016-12-29_detail_of_2016-12-23_San_Francisco_California.json", 5)
+details.scrape("output/json/2016-12-23_San_Francisco_California.json")
 
+'''
 scrape_name = glob.glob("output/raw_pickle/*")[0]
 #scraper = gmaps_subdivisions.PlaceScraper(scrape_name, "places_nearby")
 for termination_log in glob.glob("%s/*/termination_log.csv" % scrape_name):
@@ -15,3 +16,4 @@ for termination_log in glob.glob("%s/*/termination_log.csv" % scrape_name):
                               :line.find(". Coordinates")]
             print(place_type, subdivision_id)
     file_object.close()
+'''
