30f4b83c1a1d3bda773756b14e5b17ad0949bd48
ercas <edgarrimandocastro@gmail.com>
2017-02-16 09:55:49 -0500

    New MongoRedisWriter child class of MongoWriter

--- gms_io.py
+++ gms_io.py
@@ -5,10 +5,11 @@ import cPickle
 
 try:
     import pymongo
+
     class MongoWriter(object):
         """ Handles writing to a MongoDB collection
 
-        Args:
+        Attributes:
             collection: A pymongo.collection.Collection object to be written to.
         """
 
@@ -23,22 +24,96 @@ try:
 
             self.collection = pymongo.MongoClient(host)[db_name][collection_name]
 
-        def dump(self, data, dump_many = False):
+        def dump(self, data, dump_many = True):
             """ Write data to the previously defined collection
 
             Args:
                 data: A dictionary object or iterable to be written to the
                     collection.
-                dump_many: A boolean indicating whether or not data consists of one
-                    dictionary or many dictionaries.
+                dump_many: A boolean indicating whether or not data consists of
+                    one dictionary or many dictionaries.
             """
 
             if (dump_many):
                 self.collection.insert_many(data)
             else:
                 self.collection.insert_one(data)
+
+    try:
+        import redis
+
+        class MongoRedisWriter(MongoWriter):
+            """ Child class of MongoWriter that handles writing to a MongoDB
+            collection, checking against Redis for duplicates first
+
+            Attributes:
+                collection: A pymongo.collection.Collection object to be written
+                    to.
+                self.redis: A redis.StrictRedis instance that manages a
+                    connection to the Redis server.
+                self.redis_set: A string containing the name of the Redis set
+                    that stores seen place_ids.
+            """
+
+            def __init__(self, mongo_db_name, mongo_collection_name,
+                         redis_set = "seen_places", redis_db = 0,
+                         mongo_host = "localhost:27017",
+                         redis_host = "localhost", redis_port = 6379):
+                """ Initializes the MongoWriter class
+
+                Args:
+                    mongo_db_name: A string containing the name of the MongoDB
+                        database.
+                    mongo_collection_name: A string containing the name of the
+                        MongoDB collection.
+                    redis_set: A string containing the name of the Redis set
+                        that stores seen place_ids.
+                    redis_db: An integer describing which Redis database to use.
+                    mongo_host: A string containing the name of the MongoDB host
+                        and its port.
+                    redis_host: A string containing the name of the Redis host.
+                    redis_port: An integer describing the port to use on the
+                        Redis host.
+                """
+
+                MongoWriter.__init__(mongo_db_name, mongo_collection_name,
+                                     mongo_host)
+                self.redis = redis.StrictRedis(host = redis_host,
+                                               port = redis_port, db = redis_db)
+                self.redis_set = redis_set
+
+            def add_dict(self, _dict):
+                """ Add a dictionary to the MongoDB collection, checking for
+                duplicates first
+
+                Args:
+                    _dict: A dictionary containing a Google Places API JSON.
+                """
+                place_id = _dict["place_id"]
+                if (not self.redis.sismember(self.redis_set, place_id)):
+                    self.redis.sadd(self.redis_set, place_id)
+                    self.collection.insert_one(_dict)
+
+            def dump(self, data, dump_many = True):
+                """ Write data to the previously defined collection
+
+                Args:
+                    data: A dictionary object or iterable to be written to the
+                        collection.
+                    dump_many: A boolean indicating whether or not data consists
+                        of one dictionary or many dictionaries.
+                """
+                if (dump_many):
+                    for _dict in data:
+                        add_dict(_dict)
+                else:
+                    add_dict(data)
+    except:
+        print("MongoRedisWriter class unavailable; redis not found")
 except:
     print("MongoWriter class unavailable; pymongo not found")
+    print("MongoRedisWriter class unavailable; parent class MongoWriter "
+          "unavailable")
 
 class PickleWriter(object):
     """ Handles writing to a pickle file
