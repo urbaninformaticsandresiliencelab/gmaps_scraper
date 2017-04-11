6ee0f7757084576bc448945bbae65218af0145fd
ercas <edgarrimandocastro@gmail.com>
2017-04-11 00:42:27 -0400

    Added setup.py; fixed blank print

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    name = "gmaps_scraper",
+    description = "Suite of tools for scraping place information using the "
+                  "Google Maps API",
+    packages = ["gmaps_scraper"]
+)
